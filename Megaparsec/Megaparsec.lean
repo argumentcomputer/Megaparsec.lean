0aa1f7e8abe33ae219ff54642b23bd03883a9d7c
cognivore <noreply>
2022-06-09 17:00:21 +0100

    Problem: No way to write a parser
    Solution:
    
     - introduce monadparsec
     - while writing token, we need sets
     - use hashset (because it's the only set readily available in lean4 or mathlib4)
    
    Stuff is broken though for constraints like BEq

--- Megaparsec/Megaparsec.lean
+++ Megaparsec/Megaparsec.lean
@@ -1,3 +1,5 @@
+import Std.Data.HashSet
+
 namespace Megaparsec
 
 structure Pos where
@@ -10,8 +12,9 @@ structure SourcePos where
 
 class Stream (S: Type) where
   Token : Type
+  ordToken : Ord Token
+  beqToken : BEq Token
   Tokens : Type
-  ordToken  : Ord Token
   ordTokens : Ord Tokens
   tokenToChunk : Token -> Tokens
   tokensToChunk : List Token -> Tokens
@@ -23,7 +26,7 @@ class Stream (S: Type) where
 
 -- Convention, S is state, E is Error, T is token, A is return type.
 -- Convention for optimising currying is to apply E, S then M, and leave A for the last type argument.
--- Except for Result and Reply, where the order is S E A.
+-- Except for Result, Reply and ParseError where the order is S E A.
 structure PosState (S: Type) where
   pstateInput : S
   pstateOffset : Nat
@@ -34,7 +37,7 @@ inductive NonEmptyList (A: Type) where
 | nil (x: A)
 | cons (x: A) (xs: NonEmptyList A)
 
-inductive ErrorItem (T: Type) where
+inductive ErrorItem (T: Type) [BEq T] where
 | tokens (t: NonEmptyList T)
 | label (l: NonEmptyList Char)
 | eof
@@ -45,7 +48,7 @@ inductive ErrorFancy (E: Type) where
 | custom (e: E)
 
 inductive ParseError (S E: Type) [Stream S] where
-| trivial (offset: Nat) (unexpected: (ErrorItem (Stream.Token S))) (expected: List (ErrorItem (Stream.Token S)))
+| trivial (offset: Nat) (unexpected: (@ErrorItem (Stream.Token S) (Stream.beqToken S))) (expected: List (ErrorItem (Stream.Token S)))
 | fancy (offset: Nat) (expected : List (ErrorFancy E))
 
 structure State (S E: Type) [Stream S] where
@@ -59,6 +62,18 @@ abbrev Hints (T : Type) := List (ErrorItem T)
 inductive Result (S E A: Type) [Stream S] where
 | ok (x : A)
 | err (e : ParseError S E)
+
+structure Either (L: Type u) (R: Type v) where
+  left: L
+  right: R
+
+inductive Maybe (R: Type u) where
+| just (ok: R)
+| nothing
+
+inductive Surely (L: Type u) where
+| ok
+| error (err: L)
 
 structure Reply (S E A: Type) [Stream S] where
   state    : State S E
@@ -88,5 +103,35 @@ def runParsecT (E S: Type) [Stream S] (M: Type -> Type) [Monad M] (A: Type) (x: 
 
 def pMap (E S: Type) [Stream S] (M: Type -> Type) [Monad M] (U V: Type) (f: U -> V) (x: ParsecT E S M U) : ParsecT E S M V  :=
   ParsecT.mk (λ (b s cok cerr eok eerr) => (x.unParser b s (cok ∘ f) cerr (eok ∘ f) eerr))
+
+-- | Monads M that implement primitive parsers
+class MonadParsec (E S: Type) [Stream S] (M: Type -> Type) [Monad M] [Alternative M] where
+  -- | Stop parsing wherever we are, and report @err@
+  parseError (A: Type) (err: ParseError S E): M A
+  -- | If there's no input consumed by @parser@, labels expected token with name @name_override@
+  label (A: Type) (name_override: String) (parser: M A): M A
+  -- | Hides expected token error messages when @parser@ fails
+  hidden (A: Type) (parser: M A): M A :=
+    label A "" parser
+  -- | Attempts to parse with @parser@ and backtracks on failure, for arbitrary look ahead. See megaparsec docs for pitfalls:
+  -- https://hackage.haskell.org/package/megaparsec-9.2.1/docs/src/Text.Megaparsec.Class.html#try
+  attempt (A: Type) (parser: M A): M A
+  -- | Uses @parser@ to look ahead without consuming. If @parser@ fails, fail. Combine with 'attempt' if the latter is undesirable.
+  lookAhead (A: Type) (parser: M A): M A
+  -- | Succeeds if @parser@ fails without consuming or modifying parser state, useful for "longest match" rule implementaiton.
+  notFollowedBy (A: Type) (parser: M A): M Unit
+  -- | Uses @phi@ to recover from failure in @parser@.
+  withRecovery (A: Type) (phi: ParseError S E -> M A) (parser: M A): M A
+  -- | Observes 'ParseError's as they happen, without backtracking.
+  observing (A: Type) (parser: M A): M (@Either (ParseError S E) A)
+  -- | The parser at the end of the Stream.
+  eof: M Unit
+  -- |
+  token (A: Type) (matcher: Token -> Maybe A) (acc: Std.HashSet (ErrorItem Stream.Token)): M A
+
+-- #check StateT
+-- #check liftM
+-- #check "Esiet sveicināti!"
+-- #check Unit
 
 end Megaparsec
