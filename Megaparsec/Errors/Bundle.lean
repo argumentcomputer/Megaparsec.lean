925e84c6de8b6deeb00c73a47e14742844cadede
cognivore <noreply>
2022-06-22 18:47:31 +0100

    Problem: Bundle doesn't build because of missing import
    Solution:
    
     - Add StreamErrors to imports
     - Show that it builds by importing Errors.Bundle in Megaparsec.lean

--- Megaparsec/Errors/Bundle.lean
+++ Megaparsec/Errors/Bundle.lean
@@ -1,10 +1,11 @@
 import Megaparsec.Stream
+import Megaparsec.Errors.StreamErrors
 import Megaparsec.ParserState
 
 namespace Bundle
 
 structure ParseErrorBundle [stream : Stream.Stream S] (E : Type) where
-  errors : Util.NonEmptyList (@ParseError S E stream)
+  errors : Util.NonEmptyList (@StreamErrors.ParseError S E stream)
   posState : ParserState.PosState S
 
 end Bundle
