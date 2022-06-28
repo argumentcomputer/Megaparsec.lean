45e090691818b30c345801e1520dc7e6769cdc88
DanielRrr <daniel.rogozin@serokell.io>
2022-06-28 18:45:03 +0300

    Problem: Megaparsec has too many definitions which are irrelevant to parsing, they have a helping role
    Solution: All those definitions are moved to YatimaStdLib
    and imported. Such modules as RWST.lean and NEList.lean are removed.
    Now they're modules in YatimaStdLib.

--- Megaparsec/Common.lean
+++ Megaparsec/Common.lean
@@ -1,5 +1,8 @@
 import Megaparsec.Stream
 import Megaparsec.MonadParsec
+
+import YatimaStdLib
+
 /-!
 # Common token combinators
 
@@ -12,7 +15,7 @@ def string [m : Monad M] [a : Alternative M]
            [strm : Stream.Stream S] [mₚ : @MonadParsec.MonadParsec M E S m a strm]:
            strm.Tokens → M (strm.Tokens) :=
   fun expected =>
-    mₚ.tokens E S (fun x y => @BEq.beq (strm.Tokens) (@Util.ord2beq strm.Tokens strm.ordTokens) x y) expected
+    mₚ.tokens E S (fun x y => @BEq.beq (strm.Tokens) (@NEList.BEqOfOrd strm.Tokens strm.ordTokens) x y) expected
 
 -- TODO: Case-insensitive string
 
--- lakefile.lean
+++ lakefile.lean
@@ -9,6 +9,9 @@ package Megaparsec {
   }, {
     name := "LSpec",
     src := Source.git "https://github.com/yatima-inc/LSpec.git" "f71c4dd98c5735599664100acfaaa5f6238d13a3"
+  }, {
+    name := "YatimaStdLib",
+    src := Source.git "https://github.com/yatima-inc/YatimaStdLib.lean" "51bbd34e8acbd7a7a8d7b7a034adc2094f262211"
   }],
   defaultFacet := PackageFacet.oleans -- no executable is generated
 }
