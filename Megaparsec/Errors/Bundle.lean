b5d173e36170e61f8c2063a23805b90c20eac894
cognivore <66186054+cognivore@users.noreply.github.com>
2022-08-10 00:49:52 +0100

    Problem: No way to ergonomically parse strings in streams
    Solution:
    
    ParsecT refactoring:
     - Make ParsecT universal over some xi.
       - Write Alternative and Monad instances for for ParsecT
     - Define typeclass of outcomes, consisting of make and consumed?
       - Forward outcome tags via projections
     - Create instances for the two supported outcomes Consumed and Empty (the naming is kinda garbage but oh well).
     - Use these two tags (Consumed and Empty) to tag Ok and Err functions.
     - Make generic runOk and runErr that return (Consumed x Ok) ... bound to Reply type.
     - Implement runParsecT which evaluates a parser into a Reply.
     - Patch Reply to capture all the relevant types.
     - Make ParsecT runnable.
    
    Further work:
     - Turn errors into a set
     - Learn how to print errors, error bundles, etc.
    
    Discussion:
    https://zulip.yatima.io/#narrow/stream/24-yatima-tools/topic/.5BMegaparsec.5D.20Better.20error.20reporting/near/17799
    
    MonadParsec implementation:
      Move withHints and accHints into dedicated files. Make Ok and Err function signatures dedicated.
      The only ergonomics hit we took that all of these type wrappers take all of the types to Ok and Err.
      Errors.lean, Bundle.lean:
       - Use @ixhaedron's implementation of Error printing, as well as adding type parameters.
      Ok.lean:
       - Give me the current result (type gamma), and a state, and current error hints, and I'll give you back a monad over the current return value (type argument).
      Err.lean:
       - Give me the fresh parse error, and a state, and I'll give you back a monad over the current return value (type argument).
      MonadParsec.lean:
       - Imlpement MonadParsec for ParsecT using Straume.
      Common.lean:
       - Implement string common parser, which works on any string. It's just `tokens (==)`.
       - Add a `between` combinator for Seqs. It should probably be in the STDLIB!
      Main.lean:
       - Make a demonstration that it works.

--- Megaparsec/Errors/Bundle.lean
+++ Megaparsec/Errors/Bundle.lean
@@ -1,29 +1,56 @@
-import Megaparsec.Stream
-import Megaparsec.Errors.StreamErrors
+import Megaparsec.Errors.ParseError
 import Megaparsec.ParserState
 import YatimaStdLib
 
-namespace Bundle
+namespace Megaparsec.Errors.Bundle
 
-structure ParseErrorBundle [stream : Stream.Stream S] {E : Type} where
-  errors : NEList (@StreamErrors.ParseError S E stream)
-  posState : ParserState.PosState S
+open Megaparsec.ParserState
+open Megaparsec.Errors.ParseError
 
-def toBundle {S : Type} [stream : Stream.Stream S] {E : Type}
-             (s : ParserState.State S E) (errs : NEList (@StreamErrors.ParseError S E stream))
-             : @ParseErrorBundle S stream E :=
+structure ParseErrorBundle (β σ E : Type u) where
+  errors : NEList (ParseError β E)
+  posState : PosState σ
+
+-- Helper that makes necessary functions for `ParseErrorBundle.toErrorString`.
+private def makePEBfs [ToString β] [ToString E] : ((String → String) × PosState σ) → ParseError β E → ((String → String) × PosState σ)
+  | (o, pst), e =>
+    let (msline, pst') := ((.none : Option String), pst)-- reachOffset (errorOffset e) pst
+    let epos := pst'.sourcePos
+    let offendingLine := match msline with
+      | .none => ""
+      | .some sline =>
+        let elen := match e with
+          | .trivial _     none _ => 1
+          | .trivial _ (some x) _ => errorItemLength x
+          | .fancy   _         xs =>
+            xs.foldl (fun acc e => max acc (errorFancyLength e)) 1
+        let rpshift := epos.column.pos - 1
+        let pointerLen :=
+          if rpshift + elen > sline.length
+            then sline.length - rpshift + 1
+            else elen
+        let pointer := String.mk $ List.replicate pointerLen '^'
+        let lineNumber := s!"{epos.line.pos}"
+        let padding := String.mk $ List.replicate (lineNumber.length + 1) ' '
+        let rpadding :=
+          if pointerLen > 0 then String.mk $ List.replicate rpshift ' ' else ""
+        s!"{padding}|\nlineNumber | {sline}\n{padding}| {rpadding}{pointer}\n"
+    let outChunk :=
+      s!"\n{sourcePosPretty epos}:\n{offendingLine}{parseErrorTextPretty e}"
+    (o ∘ (fun x => outChunk ++ x), pst')
+
+/-
+  Pretty-print a `ParseErrorBundle`. All `ParseError`s in the bundle will
+  be pretty-printed in order together with the corresponding offending
+  lines by doing a single pass over the input stream. The rendered `String`
+  always ends with a newline.
+-/
+instance [ToString β] [ToString E] : ToString (ParseErrorBundle β σ E) where
+  toString b :=
+    let (r, _) := NEList.foldl makePEBfs (id, b.posState) b.errors
+    String.drop (r "") 1
+
+open Megaparsec.ParserState in
+def toBundle (s : State β σ E) (errs : NEList (ParseError β E))
+             : ParseErrorBundle β σ E :=
   ParseErrorBundle.mk errs s.posState
-
--- def pretty {S : Type} [stream : Stream.Stream S] {E : Type} (x : @ParseErrorBundle S stream E) : String :=
---   sorry
-
--- def prettyDo {S : Type} [stream : Stream.Stream S] {E : Type}
---              (drain : @ParseErrorBundle S stream E) (acc : List String)
---              : List String :=
---     match drain.errors with
---     | Util.NonEmptyList.cons x xs =>
---       match xs with
---       | [] => prettyDo xs prettyParserErrorWithOffset
---       | List.cons x' xs' => prettyDo
-
-end Bundle
