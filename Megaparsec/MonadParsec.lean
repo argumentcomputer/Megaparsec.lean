ee9ccee428bcedeb26adc523c111140a998fc5b5
cognivore <noreply>
2022-08-19 02:03:53 +0100

    Problem: No implementation of token / tokens
    Solution:
    
     - Implement those

--- Megaparsec/MonadParsec.lean
+++ Megaparsec/MonadParsec.lean
@@ -18,6 +18,7 @@ open Straume
 open Straume.Chunk
 open Straume.Iterator (Iterable)
 open Straume.Iterator renaming Bijection → Iterable.Bijection
+open Straume.Iterator renaming toList → Iterable.toList
 
 namespace MonadParsec
 
@@ -55,10 +56,9 @@ class MonadParsec (m : Type u → Type v) (α β ℘ E : outParam (Type u)) wher
   Otherwise, accumulate `.none`s into `acc` for error reporting. -/
   token : (φ : β → Option γ) → (acc : List (ErrorItem β)) → m γ
   /- Parse `y` tokens from the beginning of the stream, such that y = |x| > 0, if `φ x y`.
-  Succed if |x| = 0.
   Backtrack on fail.
   -/
-  tokens : (φ : α → α → Bool) → (x : α) → m γ
+  tokens : (φ : α → α → Bool) → (x : α) → m α
   /- Never fail to parse zero or more tokens with `φ`.
 
   Optional `String` is a way to name the expected token.
@@ -105,11 +105,16 @@ universe v
 
 private def hs₀ (β σ E : Type u) (_ : State β σ E) (_ : ParseError β E) : Hints β := []
 private def hs' (β σ E : Type u) (s' : State β σ E) (e : ParseError β E) := toHints (State.offset s') e
+private def nelstr (x : Char) (xs : String) := match NEList.nonEmptyString xs with
+  | .some xs' => NEList.cons x xs'
+  | .none => NEList.uno x
 
 instance theInstance {m : Type u → Type v} {α β σ E : Type u}
                      [Monad m] [Iterable α β] [Iterable.Bijection β α] [@Straume m σ Chunk α β]
                      : MonadParsec (ParsecT m β σ E) α β σ E where
+
   parseError e := fun _xi s _cok _cerr _eok eerr => eerr.2 e s
+
   label l p := fun xi s cok cerr eok eerr =>
     let el := Option.map ErrorItem.label (NEList.nonEmptyString l)
     let f x s' hs :=
@@ -123,36 +128,84 @@ instance theInstance {m : Type u → Type v} {α β σ E : Type u}
       | ParseError.trivial pos us _ => .trivial pos us (Option.option [] (fun x => [x]) el)
       | _ => err
     p xi s (cok.1, f) cerr (eok.1, g) (eerr.1, ge)
+
   attempt p := fun xi s cok _ eok eerr =>
     let forceBacktrack e _ := eerr.2 e s
     p xi s cok (Consumed.mk, forceBacktrack) eok (Empty.mk, forceBacktrack)
+
   lookAhead p := fun xi s _ cerr eok eerr =>
     let forceBacktrack x _ _ := eok.2 x s [] -- TODO: why not forward hints?
     p xi s (Consumed.mk, forceBacktrack) cerr (Empty.mk, forceBacktrack) eerr
+
   notFollowedBy p := fun xi s _ _ eok eerr => do
     let o := s.offset
     let y : (Chunk β × σ) ← Straume.take1 α s.input
     let c2e := ErrorItem.tokens ∘ NEList.uno
     let subject : ErrorItem β := match y.1 with
+    -- TODO: Here and in many other places, we have two branches that are the same because Parsec doesn't care about .fin vs .cont
+    -- TODO: Perhaps, we should use Terminable and extract values
     | .nil => ErrorItem.eof -- If by the time we call notFollowedBy the stream is empty, treat it as eof.
     | .cont c => c2e c
     | .fin (c, _reason) => c2e c -- It's ok to work with .fin, because we never consume.
-    let unexpect u := ParseError.trivial o (.some u) []
-    let ok _ _ _ := eerr.2 (unexpect subject) s
+    let ok _ _ _ := eerr.2 (.trivial o subject []) s
     let err _ _ := eok.2 PUnit.unit s []
     p xi s (Consumed.mk, ok) (Consumed.mk, err) (Empty.mk, ok) (Empty.mk, err)
+
   withRecovery φ p := fun xi s cok cerr eok eerr =>
     let err (fHs := (hs₀ β σ E)) e sFail :=
       let ok ψ := fun x s' _hs => ψ x s' (fHs s' e)
       let err _ _ := cerr.2 e sFail
       (φ e) xi sFail (cok.1, ok cok.2) (Consumed.mk, err) (eok.1, ok eok.2) (Empty.mk, err)
     p xi s cok (Consumed.mk, err) eok (Empty.mk, err $ hs' β σ E)
+
   observing p := fun xi s cok cerr eok eerr =>
     let err (fHs := (hs₀ β σ E)) e s' := cok.2 (.left e) s' (fHs s' e)
     p xi s (cok.1, cok.2 ∘ .right) (Consumed.mk, err) (eok.1, eok.2 ∘ .right) (Empty.mk, err (hs' β σ E))
-  eof := sorry
-  token := sorry
-  tokens := sorry
+
+  eof := fun xi s _ _ eok eerr => do
+      let y : (Chunk β × σ) ← Straume.take1 α s.input
+      let err c := eerr.2 (.trivial s.offset (.some $ ErrorItem.tokens $ NEList.uno c) ([.eof])) s
+      match y.1 with
+      | .nil => eok.2 PUnit.unit s []
+      | .cont c => err c
+      | .fin (c, _) => err c
+
+  token ρ errorCtx := fun xi s cok cerr eok eerr => do
+    -- TODO: Uhh, if y : γ, then we should really not call these variables "y"
+    -- In reality, they are cctx ot csctx.
+    let y : (Chunk β × σ) ← Straume.take1 α s.input
+    let test c := match ρ c with
+    | .none =>
+      eerr.2 (.trivial s.offset (.some $ ErrorItem.tokens $ NEList.uno c) errorCtx) s
+    | .some y' => cok.2 y' {s with offset := s.offset + 1, input := y.2} []
+    match y.1 with
+    | .nil => eerr.2 (.trivial s.offset (.some ErrorItem.eof) errorCtx) s
+    | .cont c => test c
+    | .fin (c, _) => test c
+
+  tokens f l := fun xi s cok cerr eok eerr => do
+    let n : Nat := Iterable.length l
+    let y : (Chunk α × σ) ← Straume.takeN n s.input
+    let unexpect pos' u :=
+      let got := pure u
+      let want := match NEList.nonEmpty (Iterable.toList l) with
+      | .none => []
+      | .some nel => [ ErrorItem.tokens nel ]
+      ParseError.trivial pos' got want
+    let test r := if f l r
+      then
+        let s' := { s with offset := s.offset + n, input := y.2 }
+        (if n == 0 then eok.2 else cok.2) r s' []
+      else
+        let got₀ := match NEList.nonEmpty (Iterable.toList r) with
+        | .none => ErrorItem.label (nelstr 'F' "ailed to parse empty input")
+        | .some nel => ErrorItem.tokens nel
+        eerr.2 (unexpect s.offset got₀) s
+    match y.1 with
+    | .nil => eerr.2 (unexpect s.offset ErrorItem.eof) s
+    | .cont cs => test cs
+    | .fin (cs, _) => test cs
+
   takeWhileP := sorry
   takeWhile1P := sorry
   takeP := sorry
