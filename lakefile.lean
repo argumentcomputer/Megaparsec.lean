53ec49d53055090912c7a5e0fa10c7a6643e148f
cognivore <noreply>
2022-08-25 05:40:43 +0100

    Problem: No ergonomic Lisp parser
    Solution:
    
    First prepare for the implementation.
    
     - Use MonadParsec-polymorphism to provide Char parsers
     - Use ParsecT-polymorphism to provide String parsers
     - The choice of polymorphic "resolution" is up to implementer, but it seems like MonadParsec-polymorphism is less wrong, because one can then provide concretisations via functions akin to `char_simple_pure` in `Char`.
     - Show demo that it works both generically, and concretely via `char_simple_pure`.
     - Added a Streamable type class that allows us to change offsets there
      and back again. Currently, it only implements String, as well as Coco-coersive
      types. We need to at least add an instance for ByteArrays, and maybe more.
      Actually added correct(ish) printing of errors!
    
    All the changes afterwards, including writing a dumb Lisp parser:
    
    1. .vscode/tasks.json for easier project build
    2.  Main.lean: more semi-automated tests and ergonomics showcase
    3. Char.lean: char-specific parsers with ergonomic exports defaulting to `String` as the corresponding composite.
    4. Common.lean: a working, but still WIP implementation of common parser combinators
    5. \*Error\*.lean: improvements by @ixahedron for printability and continuity of error reporting facilities
    6. Lisp.lean: very simple Lisp PoC that just supports parentheses and strings
    7. MonadParsec.lean: de-optionalise beta and error types, thus it's now the burden of the Megaparsec hacker to tie together composite \alpha and atomic \beta while implementing MonadParsec.
    8. Parsec.lean: @ixahedron's Streamable is now explicitly used while testing parsers (@ixahedron is it strictly needed? If yes, then we need it in all other functions that take stuff?! Can we make Streamable extend Straume so that we have a single constraint for \sigma, \alpha and \beta?)
    9. ParserState: add functionality to attach information about ranges of `SourcePos` to parsers. Related: add `withRange` runner into `MonadParsec.lean`, which is a way to run a parser, while tracking the positions in the source stream and then fold it into a parser which applies those in the error reporting machinery.
    10. README.md: add information about how to use Megaparsec.
    
    Works. Sorry for debug garbage. Gonna merge tomorrow.
    
    Commit dedicated to spacebar haters
    
    Clean up stuff, add more monadic flexibility
    
    Docs, backport sepBy and many to Common, but specify to MonadParsec / Parsec
    
    bugfix
    
    Working lisp
    
    Demo in readme.
    
    Add some words about running the parsers into README.
    
    Lean md!
    
    Prove that ergonomic file parser works.
    
    Exlpain what  returns in README.
    
    Remove broken test.

--- lakefile.lean
+++ lakefile.lean
@@ -7,10 +7,10 @@ require LSpec from git
   "https://github.com/yatima-inc/LSpec.git" @ "77fc51697abeff937ffd20d2050723dc0fa1c8c0"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "a2bbc9a48db7efd5d761a5b27f2cc6c1863b9622"
+  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "234ee564abf64c6e45f15ed921d94a3b4db228ab"
 
 require Straume from git
-  "https://github.com/yatima-inc/straume/" @ "1f007dd57e8e0da6fe966f052be1f3724bd1281a"
+  "https://github.com/yatima-inc/straume/" @ "d8b8084bee708a230379f53ff674570153a52f0d"
 
 @[defaultTarget]
 lean_exe megaparsec {
