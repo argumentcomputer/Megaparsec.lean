c86177b6417d5dae65d90ada9fd59fae0c012c4a
Ilona Prikule <ilona@yatima.io>
2022-09-15 19:55:37 +0200

    Finish errors w/ pretty-printing
    Problem: though printing errors right now kind of works, we want
    to make this even nicer by e.g. pretty-printing 'Char's, which
    make up the majority of out current usecases. This will print
    special characters in a readable manner.
    
    Solution: based on a newer version of YatimaStdLib, switched error
    printing to a new Printable type class. Also cleaned up the rest a bit.

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ require LSpec from git
   "https://github.com/yatima-inc/LSpec.git" @ "c63610bb23451c7aa2faae17c71e8d162c6c616e"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "9c362443e0d89eb96683b52a1caaf762049697c4"
+  "https://github.com/yatima-inc/YatimaStdLib.lean.git" @ "54177756e0f3488979b05153872d1832bc5ba625"
 
 require Straume from git
   "https://github.com/yatima-inc/straume/" @ "c1d72a24ae3a8a8e0bd7928001b55958e4b9113c"
