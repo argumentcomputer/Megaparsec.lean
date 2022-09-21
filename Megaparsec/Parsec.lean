6d529483c9fe0e49836cf2d7d14e95e4cf86f9b0
John C. Burnham <jcb@johnchandlerburnham.com>
2022-09-21 14:13:24 -0400

    add useful test function

--- Megaparsec/Parsec.lean
+++ Megaparsec/Parsec.lean
@@ -225,3 +225,8 @@ def parseTestP (p : Parsec β σ E γ) [ToString γ] [ToString β] [ToString E]
   match parseP p "" xs with
   | .left es => IO.println s!"{es}" >>= fun _ => pure $ (false, Either.left ())
   | .right y => IO.println y >>= fun _ => pure $ (true, Either.right y)
+
+def parseTest (p : Parsec β σ E γ) [ToString γ] [ToString β] [ToString E] (xs : σ) [Streamable σ] : String :=
+  match parseP p "" xs with
+  | .left es => s!"Err: {es}"
+  | .right y => s!"Ok: {y}"
