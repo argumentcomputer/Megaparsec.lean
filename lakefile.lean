8422cdda06f65aee4e5afaddb9b830902ef84f90
cognivore <66186054+cognivore@users.noreply.github.com>
2022-12-17 16:27:40 +0000

    Problem: Some deps have `.git`, some don't
    Solution:
    
     - Make them consistent!

--- lakefile.lean
+++ lakefile.lean
@@ -7,13 +7,13 @@ package Megaparsec
 lean_lib Megaparsec
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "89798a6cb76b2b29469ff752af2fd8543b3a5515"
+  "https://github.com/yatima-inc/LSpec" @ "89798a6cb76b2b29469ff752af2fd8543b3a5515"
 
 require YatimaStdLib from git
   "https://github.com/yatima-inc/YatimaStdLib.lean" @ "f905b68f529de2af44cf6ea63489b7e3cd090050"
 
 require Straume from git
-  "https://github.com/yatima-inc/straume/" @ "849b7c7dc5eff27bf8cfe7e9e91d6195a7b964cb"
+  "https://github.com/yatima-inc/straume" @ "849b7c7dc5eff27bf8cfe7e9e91d6195a7b964cb"
 
 @[default_target]
 lean_exe megaparsec {
