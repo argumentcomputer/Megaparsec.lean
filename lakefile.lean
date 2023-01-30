0572258b1cfee6a399d0b867a764cee97e73a254
Ilona Prikule <ilona@yatima.io>
2023-01-30 21:34:52 +0100

    bump YatimaStdLib dependency

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ require LSpec from git
   "https://github.com/yatima-inc/LSpec" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "49ee890897dbdd4665d0e8c75cd3401f0b4e6f21"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "533d71efa5853ff014f42c174d6321d74251209f"
 
 require Straume from git
   "https://github.com/yatima-inc/straume" @ "9597873f0b18a9e97b7315fb84968c55d09a6112"
