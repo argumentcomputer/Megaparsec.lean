8554410fbc2a0b75c929a3d30dcc5377ad910795
James <jamesgallicchio@gmail.com>
2023-09-12 07:17:56 -0400

    update to merged YatimaStdLib

--- lakefile.lean
+++ lakefile.lean
@@ -7,13 +7,13 @@ package Megaparsec
 lean_lib Megaparsec
 
 require LSpec from git
-  "https://github.com/lurk-lab/LSpec" @ "main"
+  "https://github.com/lurk-lab/LSpec" @ "3b6023654b917c8641ec5e626724a43380cff8f0"
 
 require YatimaStdLib from git
-  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "pull/98/head"
+  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "5b64c5150e371fb280728cbb5b7c225b9c96b348"
 
 require Straume from git
-  "https://github.com/lurk-lab/straume" @ "main"
+  "https://github.com/lurk-lab/straume" @ "053d9feccbface9a0b2c1a72447914376aac74ea"
 
 @[default_target]
 lean_exe megaparsec {
